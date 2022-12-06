9e46c3398bcb233d98d091302aaff3941479f0f4
Ahmed Aboughosen <55014628+AhmedAboughosen@users.noreply.github.com>
2022-12-06 18:38:08 +0200

    add export to library

--- example/lib/features/login/page/login_page.dart
+++ example/lib/features/login/page/login_page.dart
@@ -6,14 +6,9 @@ import 'package:live_stream/src/live_stream_listener.dart';
 import 'package:live_stream/src/live_stream_provider.dart';
 import 'package:live_stream/src/live_stream.dart';
 
-import '../../validation/login_validation.dart';
-import 'components/email_input.dart';
-import 'components/submit_button.dart';
-
 class LoginPage extends StatelessWidget {
    LoginPage({Key? key}) : super(key: key);
  static var liveStream = LoginLiveStream(
-      loginValidation: LoginValidation(),
       loginRepository: LoginRepositoryImpl());
 
   @override
@@ -54,7 +49,7 @@ class LoginPage extends StatelessWidget {
                 SizedBox(
                   height: 30,
                 ),
-                EmailInput(),
+                // EmailInput(),
                 // SizedBox(
                 //   height: 30,
                 // ),
@@ -66,6 +61,82 @@ class LoginPage extends StatelessWidget {
               ],
             ),
           )),
+    );
+  }
+}
+
+//
+// class EmailInput extends StatelessWidget {
+//   const EmailInput({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) =>
+//       LiveStreamBuilder<LoginLiveStream,String>(
+//         state: LiveStreamProvider.of<LoginLiveStream>(context)
+//             .loginValidation
+//             .email,
+//         liveStream: LoginPage.liveStream,
+//         builder: (BuildContext context, StreamState state) {
+//           return TextField(
+//             keyboardType: TextInputType.emailAddress,
+//             onChanged: (LiveStreamProvider.of<LoginLiveStream>(context))
+//                 .loginValidation
+//                 .email
+//                 .valueChange,
+//             decoration: InputDecoration(
+//                 labelText: "Email address",
+//                 hintText: "you@example.com",
+//                 errorText: state.error != null ? "${state.error}" : null),
+//           );
+//         },
+//       );
+// }
+
+// import 'package:example/features/login/liveStream/login_live_stream.dart';
+// import 'package:flutter/material.dart';
+// import 'package:live_stream/src/live_stream_builder.dart';
+// import 'package:live_stream/src/live_stream_provider.dart';
+// import 'package:live_stream_base/live_stream.dart';
+//
+// class PasswordInput extends StatelessWidget {
+//   const PasswordInput({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) =>
+//       LiveStreamBuilder<LoginLiveStream, String>(
+//         state: LiveStreamProvider.of<LoginLiveStream>(context)
+//             .loginValidation
+//             .password,
+//         builder: (BuildContext context, StreamBase<String?> state) {
+//           return TextField(
+//             keyboardType: TextInputType.visiblePassword,
+//             onChanged: (LiveStreamProvider.of<LoginLiveStream>(context))
+//                 .loginValidation
+//                 .email
+//                 .valueChange,
+//             decoration: InputDecoration(
+//                 labelText: "Password ",
+//                 hintText: "*******",
+//                 errorText: state.hasError ? "${state.error}" : null),
+//           );
+//         },
+//       );
+// }
+
+class SubmitButton extends StatelessWidget {
+  const SubmitButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        LiveStreamProvider.of<LoginLiveStream>(context).login();
+      },
+      color: Colors.blue,
+      child: const Text(
+        "Login",
+        style: TextStyle(color: Colors.white),
+      ),
     );
   }
 }
--- lib/live_stream.dart
+++ lib/live_stream.dart
@@ -1,7 +1,13 @@
 library live_stream;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:live_stream/src/live_stream.dart';
+export 'package:live_stream/src/live_stream_provider.dart';
+export 'package:live_stream/src/live_stream_builder.dart';
+export 'package:live_stream/src/live_stream_listener.dart';
+export 'package:live_stream/src/property.dart';
+export 'package:live_stream/src/multi_live_stream_listener.dart';
+export 'package:live_stream/src/multi_live_stream_provider.dart';
+export 'package:live_stream/src/object.dart';
+export 'package:live_stream/src/stream/async_live_stream.dart';
+export 'package:live_stream/src/stream/value_live_stream.dart';
+export 'package:live_stream/src/error/errors.dart';
